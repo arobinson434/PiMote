facd46a8aa2499425e0d47e367a162988dd2a94e
Aaron Robinson <aaron.h.robinson@gmail.com>
2025-01-11 22:18:52 -0600

    Fix multicast issues

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,8 @@ import 'package:pi_mote/models/emitters_model.dart';
 import 'package:pi_mote/viewmodels/emitters_viewmodel.dart';
 import 'package:pi_mote/views/emitters_listview.dart';
 
+import 'package:android_multicast_lock/android_multicast_lock.dart';
+
 class PiMoteHomePage extends StatefulWidget {
   const PiMoteHomePage({super.key});
 
@@ -12,6 +14,18 @@ class PiMoteHomePage extends StatefulWidget {
 
 class _PiMoteHomePageState extends State<PiMoteHomePage> {
   final EmittersViewModel viewmodel = EmittersViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    AndroidMulticastLock().acquire();
+  }
+
+  @override
+  void dispose() {
+    AndroidMulticastLock().release();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
